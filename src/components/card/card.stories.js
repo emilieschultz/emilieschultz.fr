36256fc60a465781be74c38232447532f7334ca2
emilieschultz <mail@emilieschultz.fr>
2020-10-27 22:04:00 +0100

    wip

--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -1,38 +1,70 @@
 import React from 'react';
 
-import Card from './index';
+import Card, { CardList, CardDefault } from './index';
 
 export default {
   title: 'Components/Card',
   component: Card
 };
 
-const Template = args => (
+const TemplateSmall = Component => args => (
   <div style={{ width: '250px' }}>
-    <Card {...args} />
+    <Component {...args} />
   </div>
 );
 
-const TemplateTall = args => (
+const TemplateLarge = Component => args => (
   <div style={{ width: '250px', height: '400px' }}>
-    <Card {...args} />
+    <Component {...args} />
   </div>
 );
 
-export const CardPrimary = Template.bind({});
-CardPrimary.args = {
+export const CardAbout = TemplateSmall(CardDefault).bind({});
+CardAbout.args = {
   title: 'Qui suis-je ?',
+  useChevron: true, //require('./img/chevron.svg'),
   content:
     'Si vous souhaitez en apprendre davantage sur moi, sur ma manière de travailler, mes passions et mes objectifs, c’est ici que ça se passe !',
-  smallImg: require('./img/profil-pic.png')
+  smallImg: require('./img/profil-pic.png'),
+  titleColor: 'green'
 };
 
-export const CardSecondary = TemplateTall.bind({});
-CardSecondary.args = {
+export const CardStudies = TemplateLarge(CardDefault).bind({});
+CardStudies.args = {
+  title: 'Mes formations',
+  useChevron: true,
+  content:
+    'Dans cette section je vous liste les différentes formations que j’ai accompli ces dernières années et qui m’ont permis de devenir qui je suis aujourd’hui.',
+  largeImg: require('./img/profil-pic.png'),
+  titleColor: 'white',
+  backgroundColor: 'purple',
+  textColor: 'white'
+};
+
+export const CardPortfolio = TemplateLarge(CardDefault).bind({});
+CardPortfolio.args = {
   title: 'Qui suis-je ?',
+  useChevron: true,
   content:
     'Si vous souhaitez en apprendre davantage sur moi, sur ma manière de travailler, mes passions et mes objectifs, c’est ici que ça se passe !',
   largeImg: require('./img/profil-pic.png')
+};
+
+export const CardSkills = TemplateLarge(CardDefault).bind({});
+CardSkills.args = {
+  title: 'Qui suis-je ?',
+  titleColor: 'purple',
+  useChevron: true,
+  content:
+    'Si vous souhaitez en apprendre davantage sur moi, sur ma manière de travailler, mes passions et mes objectifs, c’est ici que ça se passe !',
+  largeImg: require('./img/profil-pic.png')
+};
+
+export const CardList1 = TemplateSmall(CardList).bind({});
+CardList1.args = {
+  title: 'Qui suis-je ?',
+  backgroundColor: 'green',
+  titleColor: 'white'
 };
 
 // export const Secondary = Template.bind({});
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import styles from './style.module.scss';
 
-function Card({ title, content, smallImg, largeImg }) {
+function Card({
+  title,
+  children,
+  useChevron = false,
+  titleColor = 'green',
+  backgroundColor = 'white'
+}) {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>
+    <div className={`${styles.container} ${styles[backgroundColor]}`}>
+      <h2 className={`${styles.title} ${styles[titleColor]}`}>
         {title}
-        <img src={require('./img/chevron.svg')} alt="" />
+        {!!useChevron && <img src={require('./img/chevron.svg')} alt="" />}
       </h2>
-      <p className={styles.content}>{content}</p>
-      {!!smallImg && <img src={smallImg} width="60" alt="" />}
-      {!!largeImg && <img src={largeImg} width="100%" alt="" />}
+      {children}
     </div>
   );
 }
+
+export const CardDefault = ({
+  content,
+  smallImg,
+  largeImg,
+  textColor = 'black',
+  ...otherProps
+}) => (
+  <Card {...otherProps}>
+    <p className={`${styles.content} ${styles[textColor]}`}>{content}</p>
+    {!!smallImg && <img src={smallImg} width="60" alt="" />}
+    {!!largeImg && <img src={largeImg} width="100%" alt="" />}
+  </Card>
+);
+
+export const CardList = ({ ...otherProps }) => (
+  <Card {...otherProps}>
+    <ul>
+      <li>Réalisation</li>
+      <li>Création de logotypes</li>
+    </ul>
+  </Card>
+);
 
 export default Card;
