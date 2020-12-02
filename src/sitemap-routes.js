3195da712425f1bd7a95bf52e91627a5ee4f49d3
emilieschultz <mail@emilieschultz.fr>
2020-12-02 21:11:21 +0100

    add sitemap

---
+++ src/sitemap-routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/qui-suis-je" />
+    <Route path="/formations" />
+    <Route path="/portfolio" />
+    <Route path="/competences" />
+  </Route>
+);
